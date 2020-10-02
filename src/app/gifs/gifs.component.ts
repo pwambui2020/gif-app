4b51782ec38f2c58a67973af492009ba706829ef
Paulyne <paulynewambui2020@gmail.com>
2020-10-02 15:05:07 +0300

    add the gifs component

---
+++ src/app/gifs/gifs.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-gifs',
+  templateUrl: './gifs.component.html',
+  styleUrls: ['./gifs.component.css']
+})
+export class GifsComponent implements OnInit, OnDestroy {
+  gifs: any[] = [];
+  subscription: Subscription;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getTrendingGIFs();
+    this.subscription = this.dataService.getGifs()
+      .subscribe((response: any) => {
+        this.gifs = response;
+      });
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
