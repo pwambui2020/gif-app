38fc6aeb93041cddc0491ea091ed26009503f5a4
Paulyne <paulynewambui2020@gmail.com>
2020-10-04 12:40:13 +0300

    add the github service

---
+++ Documents/angular-apps/github-api/src/app/service/github.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+     
+    const url ="https://api.github.com/users"
+
+    return this.http.get<any>(url)
+
+  }
+ 
+  }
+
